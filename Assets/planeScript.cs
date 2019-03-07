5d182287351be9d98435d7a21285530af8d86ab7
Abhishek0706 <patil.abhishek12@rediffmail.com>
2019-03-08 03:59:48 +0530

    changed color theme

---
+++ Assets/planeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0, 8, 0);
+    }
+}
